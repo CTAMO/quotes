be0e8dae4fbfb87d10d538e27a3007aec986f92a
CTAMO <johndoe2016@abv.bg>
2015-01-17 13:26:32 +0200

    Test commit 4

--- test/server/server-tests.js
+++ test/server/server-tests.js
@@ -4,7 +4,7 @@ var exports = require("../../app/server-generator.js");
 var numberGenerator = exports.numberGenerator;
 
 describe("Server suite", function() {
-    it("Expect 5 to be 5", function() {
-        expect(numberGenerator.getNumber()).to.equal(5); 
+    it("Expect 5 to be 5", function() { 
+        expect(numberGenerator.getNumber()).to.equal(5);
     });
 });
